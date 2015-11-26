562bdb1700a11ab7750242849d510fc033ad60ac
Łukasz Dąbrowski <email@lukaszdabrowski.com>
2015-11-26 18:21:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# UrlFragmentIdentifierCleaner
+Simple ASP.NET 5 web appication demonstrating how to hide url fragment from the address bar by using JS library browser-utility-0.0.1.js
+
+Developed and tested using VS 2015
+<br />
+After application is up and running, there is menu at the top of the page that is self-describing.
+<br />
+After zip is downloaded && extracted, open solution, close it, and reopen once again.
+Then you can start the application.
---
+++ src/UrlFragmentIdentifierCleaner/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace UrlFragmentIdentifierCleaner.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HideUrlFragment()
+        {
+            return View();
+        }
+
+        public IActionResult ShowUrlFragment()
+        {
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
